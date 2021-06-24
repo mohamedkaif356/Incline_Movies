4f4d9d7fc43688f608e8bb773c7d3ff44b0cdbf6
Mohamed Kaif <52997693+mohamedkaif356@users.noreply.github.com>
2021-06-24 17:50:45 +0530

    Update Constants.kt

--- InclineMovies/app/src/main/java/com/example/inclinemovies/data/Constants.kt
+++ InclineMovies/app/src/main/java/com/example/inclinemovies/data/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val API_KEY = "36b3263e43d44de472403d3147b49cd1"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+    const val MY_NAME = "Mohamed"
 
 }
