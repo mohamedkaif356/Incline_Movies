7ac655bc35694ea3383b941361f426667ae4690b
Mohamed Kaif <mohamedkaif525@gmail.com>
2021-06-25 17:04:19 +0530

    Added Search NetworkCheck

--- InclineMovies/app/src/main/java/com/example/inclinemovies/ui/AllMoviesViewModel.kt
+++ InclineMovies/app/src/main/java/com/example/inclinemovies/ui/AllMoviesViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.inclinemovies.ui
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.inclinemovies.data.MoviesRepository
+import com.example.inclinemovies.data.moviesresponse.Result
+import kotlinx.coroutines.launch
 
 class AllMoviesViewModel : ViewModel() {
+
+    private val _popularMovies = MutableLiveData<List<Result>>()
+    val popularMovies : LiveData<List<Result>> = _popularMovies
+
+    private val mr = MoviesRepository()
+
+    fun fetchPopularMovies() = viewModelScope.launch {
+        mr.getPopularMovies().body()?.let {
+            _popularMovies.postValue(it.results)
+        }
+    }
 
 }
--- InclineMovies/app/src/main/java/com/example/inclinemovies/ui/MainActivity.kt
+++ InclineMovies/app/src/main/java/com/example/inclinemovies/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.inclinemovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
 import com.example.inclinemovies.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(AllMoviesViewModel::class.java)
+
+        val fetchMovies = findViewById<Button>(R.id.fetch_movies)
+
+        fetchMovies.setOnClickListener {
+            viewModel.fetchPopularMovies()
+        }
     }
 }
